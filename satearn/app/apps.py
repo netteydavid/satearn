c79810a145eb0a2cb4ea33f0d55ab12ca6192ae4
netteydavid <netteydavid@live.com>
2021-12-12 00:59:48 -0600

    Making simple app as MVP

--- satearn/app/apps.py
+++ satearn/app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SatearnApiConfig(AppConfig):
+class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'satearn_api'
+    name = 'app'
