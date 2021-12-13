cb7580e935e403fd8271929e7973478511a82151
netteydavid <netteydavid@live.com>
2021-12-12 21:52:02 -0600

    Added models

--- satearn/app/models.py
+++ satearn/app/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from datetime import datetime
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+# Represents a bounty created by a user and completed by another
+class Bounty(models.Model):
+
+    # Status constants
+    NEW = 'NEW'
+    WORKING = 'WORKING'
+    COMPLETE = 'COMPLETE'
+    CANCELLED = 'CANCELLED'
+    # The statuses a bounty can take on
+    BOUNTY_STATUS = [
+        (NEW, 'New'), 
+        (WORKING, 'Working'), 
+        (COMPLETE, 'Complete'), 
+        (CANCELLED, 'Canceled')
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    reward = models.IntegerField()
+    due_date = models.DateTimeField()
+    created_on = models.DateTimeField(default=datetime.now(), editable=False)
+    status = models.CharField(max_length=10, choices=BOUNTY_STATUS, default=NEW)
+    assigned_to = models.ForeignKey(
+        get_user_model(), 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        related_name='assignments', 
+        related_query_name='assignment'
+    )
+    completed_on = models.DateTimeField(null=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+# Represents an invoice created to pay for a bounty
+class Invoice(models.Model):
+    invoice = models.CharField(max_length=200)
+    bounty = models.ForeignKey(Bounty, on_delete=models.SET_NULL, null=True)
+    created_on = models.DateTimeField(default=datetime.now(), editable=False)
+
+    def __str__(self) -> str:
+        return self.invoice
