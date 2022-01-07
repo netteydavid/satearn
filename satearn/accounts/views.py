f9ab06a6d7f23eec538893ddc2291d8d484259ea
netteydavid <netteydavid@live.com>
2022-01-06 21:28:21 -0600

    User edit form and profile edit form

--- satearn/accounts/views.py
+++ satearn/accounts/views.py
@@ -22,6 +22,11 @@ def user(request):
     return render(request, "registration/user.html", {"user": request.user, "user_form": user_form})
 
 def profile(request, user_id):
+    if request.method == 'POST':
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            return redirect('accounts:profile', user_id=user_id)
     user = get_object_or_404(get_user_model(), pk=user_id)
     profile_form = ProfileForm(instance=user.profile)
     return render(request, "registration/profile.html", {"is_me": request.user.id == user_id, "user": user, "profile_user": profile_form})
